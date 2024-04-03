55887af7e06214de46d0978ed9d4afcbbec5ab51
Dhudakonda Mamatha <141036025+DhudakondaMamatha@users.noreply.github.com>
2024-04-03 19:01:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for Portfolio Website
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+    // You can add code here to handle form submission, e.g., sending form data to a server
+    alert('Form submitted successfully!');
+});
